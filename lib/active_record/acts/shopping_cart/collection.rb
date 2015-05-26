9687ea6f1dcdf9d93e2c8b8d73ad7675b2328894
Leo Li <lxz.tty@gmail.com>
2015-05-27 03:54:31 +1000

    0.3.1 change the 'cumulative' meaning: - original: true, add same item into shopping_cart will increment the quantity, false: no - changed: true, add same item into shopping_cart will increment the quantity, false: create another shopping_cart_item record

--- lib/active_record/acts/shopping_cart/collection.rb
+++ lib/active_record/acts/shopping_cart/collection.rb
@@ -4,18 +4,16 @@ module ActiveRecord
       module Collection
         #
         # Adds a product to the cart
-        #
+        # cumulative = true, update quantity of existing item or create new one if no existing
+        # cumulative = false, create a new one in any cases
         def add(object, price, quantity = 1, cumulative = true)
-          cart_item = item_for(object)
-
-          if cart_item
-            cumulative = cumulative == true ? cart_item.quantity : 0
-            cart_item.quantity = (cumulative + quantity)
+          if cumulative && (cart_item = item_for(object))
+            cart_item.quantity += quantity
             cart_item.save
             cart_item
           else
             shopping_cart_items.create(item: object, price: price, quantity: quantity)
-          end
+          end          
         end
 
         #
--- lib/acts_as_shopping_cart/version.rb
+++ lib/acts_as_shopping_cart/version.rb
@@ -1,3 +1,3 @@
 module ActsAsShoppingCart
-  VERSION = "0.3.0"
+  VERSION = "0.3.1"
 end
